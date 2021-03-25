37ffb13315cfbb9f5c45713350174b3d0a1cd581
brandnn07 <brandcarter4@gmail.com>
2021-03-25 14:15:37 -0400

    worked on routes, finished auth.js

---
+++ controllers/home-routes.js
@@ -0,0 +1,34 @@
+const router = require('express').Router();
+const { Post, User} = require('../models');
+const withAuth = require('../utils/auth');
+
+
+// Gets info from posts onto dashboard
+router.get('/', withAuth, async (req, res) => {
+    try {
+        const dashboardData = await Post.findAll({
+            include: [
+                {
+                    attributes: ['title', 'post_content', 'date_created'] ,
+                },
+            ],
+        });
+        const posts = dashboardData.map((post) => 
+            post.get({ plain: true})
+        );
+
+        res.render('dashboard', {
+            posts,
+            loggedIn: req.session.loggedIn,
+        });
+    } catch(err) {
+        console.log(err);
+        res.status(500).json(err);
+    }
+});
+
+
+// Paths to settings
+router.get('/settings', withAuth, async (req, res) => {
+    
+})
