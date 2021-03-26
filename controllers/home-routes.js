d9e6cae69e5ba95966b71f82b0dd6e8420287ab8
brandnn07 <brandcarter4@gmail.com>
2021-03-26 13:37:36 -0400

    im not sure

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -17,7 +17,7 @@ router.get('/', withAuth, async (req, res) => {
             post.get({ plain: true})
         );
 
-        res.render('dashboard', {
+        res.render('login', {
             posts,
             loggedIn: req.session.loggedIn,
         });
